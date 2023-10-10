b51a503c0d5ac5eaae9e382103362e0e77c4dda3
amandadebussy <amanda.debussy.di@gmail.com>
2023-10-10 10:40:45 -0300

    new basic project

---
+++ script.js
@@ -0,0 +1,10 @@
+var textarea = document.querySelector("#btn")
+document.addEventListener("click", function() {
+    var text = document.getElementById("box-text").value;
+    var text1 = text.length;
+    document.getElementById("result").innerHTML = text1;
+
+})
+
+
+//nao mexe nisso aqui nao hein! to de olho!!!1
