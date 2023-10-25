135edb24149011d713484b1d8f8d9d8193bf2084
amandadebussy <amanda.debussy.di@gmail.com>
2023-10-25 08:56:29 -0300

    new commit

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-var textarea = document.querySelector("#btn")
-document.addEventListener("click", function() {
-    var text = document.getElementById("box-text").value;
-    var text1 = text.length;
-    document.getElementById("result").innerHTML = text1;
+var text = document.querySelector("#box-text");
+var result = document.getElementById("result");
+var button = document.querySelector("#btn");
+
+
+text.addEventListener("input", function() {
+    var textValue = text.value.trim();
+    var textLength = textValue.length;
+
+    if (textLength > 100) {
+        text.value = textValue.slice(0, 100); // Limita o texto a 10 caracteres
+        textLength = 100; // Atualiza o comprimento para 10
+    }
+
+    result.textContent = textLength;
+    document.getElementById("result2").innerHTML = textValue;
+});
+
+
+
+
+//criando um evento de remover caracter
+button.addEventListener("click", () => {
+    text.value = ""
+
+
 
 })
+
+
+
+
+
+
+
 
 
 //nao mexe nisso aqui nao hein! to de olho!!!1
